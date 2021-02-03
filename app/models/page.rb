d512ea7c3e2edeea7d145cd4c6d9f61c1bbfbecf
othman <othmanistaiteh@gmail.com>
2021-02-03 16:21:58 +0200

    Finish 8

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,5 +1,5 @@
 class Page < ActiveRecord::Base
     has_and_belongs_to_many :editors, :class_name => "AdminUser"
     belongs_to :subject
-    has_many :subjects
+    has_many :sections
 end
--- app/models/section_edit.rb
+++ app/models/section_edit.rb
@@ -1,4 +1,4 @@
 class SectionEdit < ApplicationRecord
-    belongs_to :admin_user
+    belongs_to :editor, :class_name => "AdminUser", :foreign_key => "admin_user_id"
     belongs_to :section
 end
